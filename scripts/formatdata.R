f0b4c4e83f8496f679510b96e1610c8181f10fa4
hrecht <hrecht@users.noreply.github.com>
2015-08-19 15:13:03 -0400

    add wireframe

--- scripts/formatdata.R
+++ scripts/formatdata.R
@@ -80,3 +80,5 @@ all = left_join(s1,s2,by=c("year","category","catval", "percentile"))
 all = left_join(all,s3,by=c("year","category","catval", "percentile"))
 all = left_join(all,s4,by=c("year","category","catval", "percentile"))
 write.csv(all, "data/allscenarios.csv",na="",row.names = F)
+
+all$temp = (all$wealth_3 - all$wealth_4)
